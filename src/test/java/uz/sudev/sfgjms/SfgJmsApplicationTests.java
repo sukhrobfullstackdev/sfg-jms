487d4fc32150baaae46e973952a3036977d06e01
sukhrobdev <suxrobdevbk@gmail.com>
2022-11-16 17:56:22 +0500

    Created a project for JMS

---
+++ src/test/java/uz/sudev/sfgjms/SfgJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.sudev.sfgjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
